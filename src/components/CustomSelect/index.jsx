82f6a799d5046dfc38b5c84a20f953955af2e148
Jones-Mendes <jonesoliver99@hotmail.com>
2025-05-07 12:00:40 -0300

    Dark_Filmes

--- src/components/CustomSelect/index.jsx
+++ src/components/CustomSelect/index.jsx
@@ -1,8 +1,10 @@
-export default function CustomSelect({label, options}){
+export default function CustomSelect({label, options, onChange}){
     return (
         <div className="wfull flex flex-col gap-2">
             <label htmlFor="text[17px] font-bold">{label}</label>
-            <select className="w-full h-[45px] bg-[#141414] rounded-lg pl-2 border border-[#ffffff1a] outline-none focus:border-purple-400">
+            <select
+            onChange={onChange}
+            className="w-full h-[45px] bg-[#141414] rounded-lg pl-2 border border-[#ffffff1a] outline-none focus:border-purple-400">
                 <option value="">Selecione...</option>
                 {options.map((opcao)=>{
                     return (    
--- src/pages/cadastrar/index.jsx
+++ src/pages/cadastrar/index.jsx
@@ -1,71 +1,161 @@
-import CustonInput from "@/components/CustomInput";
+import instance from "@/api/instance";
+import CustomInput from "@/components/CustomInput";
 import CustomSelect from "@/components/CustomSelect";
-import Header from "@/components/Header";
-import PageWrapper from "@/components/pageWrapper";
+import PageWrapper from "@/components/PageWrapper";
+import { useState } from "react";
 
 export default function Cadastrar(){
+    const [titulo, setTitulo] = useState("");
+    const [diretor, setDiretor] = useState("");
+    const [ano, setAno] = useState(0);
+    const [genero, setGenero] = useState("");
+    const [nota, setNota] = useState(0);
+    const [sinopse, setSinopse] = useState("");
+    const [banner, setBanner] = useState("");
+
+    async function handleSubmit(event){
+        event.preventDefault();
+        try {
+            await instance.post("/api/movies", {
+                titulo: titulo,
+                diretor: diretor,
+                ano: ano,
+                genero: genero,
+                nota: nota,
+                sinopse: sinopse,
+                banner: banner,
+            })
+
+
+            toast.success("Filme cadastrado com sucesso!")
+            setTitulo("");
+            setDiretor("");
+            setAno("");
+            setGenero("");
+            setNota("");
+            setSinopse("");
+            setBanner("");
+
+        } catch (error) {
+            console.error(error)
+        }
+                   
+    }
+
     return (
-        <PageWrapper showButton={false}>
+        <PageWrapper  showButton={false}>
             <div className="w-full h-full py-[40px] px-[70px] flex flex-col">
-                <h1 className="text-[30px] font-bold text-[#9a86f4]">Adicione um novo filme</h1>
+                <h1 className="text-[40px] font-bold text-[#9A86F4]">Adicione um novo filme</h1>
                 <p className="text-[20px] text-gray-500">Preencha o formulário abaixo para adicionar um novo filme à sua coleção.</p>
                 <div className="w-full flex justify-center pt-[40px]">
-                    <form className="w-[50%] h-auto min-h-[200px] bg-[#222222] rounded-2xl border border-[#3a364c] flex flex-col p-6 gap-4">
-                    <CustonInput 
+                    <form 
+                        onSubmit={handleSubmit}
+                        className="w-[50%] h-auto min-h-[200px] bg-[#222222] rounded-2xl border border-[#3a364c] flex flex-col p-6 gap-4">
+                        
+                        
+                        <CustomInput 
                             label="Título"
+                            value={titulo}
                             placeholder="Digite o título do filme"
                             type="text"
+                            onChange={
+                                (event) => setTitulo(event.target.value)
+                            }
                         />
-                        <CustonInput 
+                        <CustomInput 
                             label="Diretor"
+                            value={diretor}
                             placeholder="Digite o nome do diretor"
                             type="text"
+                            onChange={
+                                (event) => setDiretor(event.target.value)
+                            }
                         />
                         <div className="w-full flex gap-4">
                             <div className="w-[50%]">
-                                <CustonInput 
+                                <CustomInput
                                     label="Ano"
+                                    value={ano}
                                     placeholder="Digite o ano de lançamento"
                                     type="number"
-                        />
+                                    onChange={
+                                        (event) => setAno(event.target.value)
+                                    }
+                                />
                             </div>
                             <div className="w-[50%]">
                                 <CustomSelect
-                                label="Gênero"
-                                options={[
-                                    "Ação", 
-                                    "Comédia", 
-                                    "Drama", 
-                                    "Terror", 
-                                    "Ficção Cientifica",
-                                    "Romance"]}
+                                    value={genero}
+                                    onChange={
+                                        (event) => setGenero(event.target.value)
+                                    }
+                                    label="Gênero"
+                                    options={[
+                                        "Ação", 
+                                        "Comédia", 
+                                        "Drama", 
+                                        "Terror", 
+                                        "Ficção Científica", 
+                                        "Romance"
+                                    ]}
                                 />
                             </div>
                         </div>
-                        <div className="w-[30%] itenms-center flex gap-2">
+                        <div className="w-[30%] items-center flex gap-2">
                             <div className="w-[70%]">
-                            <CustonInput
-                                label="Nota"
-                                placeholder="0"
-                                type="number"
-
-
-                            />
+                                <CustomInput
+                                    value={nota}
+                                    onChange={
+                                        (event) => setNota(event.target.value)
+                                    }
+                                    label="Nota"
+                                    placeholder="0"
+                                    type="number"
+                                />
                             </div>
-                            <p className="flex mt-8"> 10 / 10</p>
+                            <p className="flex mt-8">10 / 10</p>
                         </div>
-                        <div className="w-full h-[150px]flex flex-col gap-2">
-                            <label htmlFor="text-[17px] font-bold">Sinopse</label>
-                            <textarea className="w-full min-h-[150px] max-h-[150px] rounded-b-lg border border-[#ffffff1a] focus:border-purple-400 p-2 bg-[#141414] outline-none" ></textarea>
+                        <div className="w-full flex flex-col">
+                            <label className="text-[17px] font-bold">Sinopse</label>
+                            <textarea
+                                value={sinopse}
+                                onChange={
+                                    (event) => setSinopse(event.target.value)
+                                }
+                                className="
+                                    w-full h-[150px] min-h-[150px] max-h-[150px]
+                                 rounded-lg border border-[#ffffff1a]
+                                 focus:border-purple-400 p-2 bg-[#141414] 
+                                 outline-none"
+                            ></textarea>
                         </div>
-                        <CustonInput
+                        <CustomInput
+                            value={banner}
+                            onChange={
+                                (event) => setBanner(event.target.value)
+                            }
                             label="Banner"
-                            placeholder="Url da Imagem"
+                            placeholder="URL da imagem"
                             type="text"
                         />
-                        <div className="w-full flex justify-end gap-4"><button type="reset" className="w-[100px] h-[50px] rounded-me bg-#141414] cursor-pointer hover:bg-white/10
-                            border border-[#3a364c]">Cancelar</button></div>
-                            
+                        <div className="w-full flex justify-end gap-4">
+                            <button 
+                                type="reset" 
+                                className="w-[100px] h-[50px] rounded-md
+                                 bg-[#141414] cursor-pointer
+                                 hover:bg-white/10
+                                border border-[#3a364c]">
+                                    Cancelar
+                            </button>
+                            <button 
+                                type="submit"
+                                className="w-[130px] h-[50px] rounded-md
+                                 bg-[#9A86F4] cursor-pointer font-bold
+                                 hover:bg-[#9A86F4]/80"
+                            >
+                                    Salvar Filme
+                            </button>
+                        </div>       
                     </form>
                 </div>
             </div>
